7256a818f42369fba97b648c9ede18fa31864bf3
Cix <gabrielviniciodacosta@gmail.com>
2023-08-10 09:10:17 -0300

    #

--- js/script.js
+++ js/script.js
@@ -3,11 +3,13 @@ const form = document.getElementById("form-itens");
 const itensInput = document.getElementById("receber-item");
 form.addEventListener("submit", function (evento) {
     evento.preventDefault();
+    salvarItem()
 });
 
 function salvarItem() {
     const comprasItem = itensInput.value;
     const checarDuplicado = listaDeItens.some((elemento) => elemento.valor === comprasItem);
+    //bolean true or false
     if (checarDuplicado) {
         alert("O item já existe")
     } else {
@@ -16,4 +18,5 @@ function salvarItem() {
             valor: comprasItem
         })
     }
+    console.log(listaDeItens)
 }

87ebc4694a8c78ec3bf8860a7c0f7b756ae65690
Cix <gabrielviniciodacosta@gmail.com>
2023-08-10 08:13:37 -0300

    #

---
+++ js/script.js
@@ -0,0 +1,30 @@
+//Array 'não recomendado'
+//let listaDeCompras = ['Arroz', 'Bolacha','Suco']
+
+//Objeto 'recomendado'
+/*const listaDeCompras = {
+    item1: 'Arroz',
+    item2: 'Bolacha',
+    item3: 'Suco',
+    quantidade1: 3,
+    quantidade2: 4,
+    quantidade2: 5,
+    mostrarLista: function () {
+        alert('Comprei ' + listaDeCompras.quantidade1 + ' pacotes de ' + listaDeCompras.item2)
+    }
+}
+listaDeCompras.mostrarLista()
+
+A linguagem JavaScript é do tipo orientada a objetos, 
+isso significa que, objetos são a parte central dessa linguagem, dessa forma, 
+é importante entender como manipular as informações que um objeto guarda.
+Também é possível adicionar propriedades a um objeto:
+const cliente = { nome: ‘Mariana’ }
+cliente.idade = 35
+console.log(cliente) 
+o console irá retornar: 
+   nome: 'Mariana', 
+   idade: 35
+*/
+
+let listaDeItens = []

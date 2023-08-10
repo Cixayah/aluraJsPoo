e64f9a72015fd3a47170a436ee4bff1958e8f8fc
Cix <gabrielviniciodacosta@gmail.com>
2023-08-10 11:24:19 -0300

    #

--- js/script.js
+++ js/script.js
@@ -1,25 +1,20 @@
 let listaDeItens = []
-const form = document.getElementById("form-itens");
-const itensInput = document.getElementById("receber-item");
-const ulItens = document.getElementById("lista-de-itens");
-const ultItensComprados = document.getElementById("itens-comprados");
-
+const form = document.getElementById("form-itens")
+const itensInput = document.getElementById("receber-item")
+const ulItens = document.getElementById("lista-de-itens")
+const ulItensComprados = document.getElementById("itens-comprados")
 form.addEventListener("submit", function (evento) {
-    evento.preventDefault();
-    salvarItem();
-    mostrarItem();
-    itensInput.focus();
-});
-
+    evento.preventDefault()
+    salvarItem()
+    mostrarItem()
+    itensInput.focus()
+})
 function salvarItem() {
-    const comprasItem = itensInput.value;
-    const checarDuplicado = listaDeItens.some((elemento) =>
-        elemento.valor.toUpperCase() === comprasItem.toUpperCase());
-    //bolean true or false
+    const comprasItem = itensInput.value
+    const checarDuplicado = listaDeItens.some((elemento) => elemento.valor.toUpperCase() === comprasItem.toUpperCase())
     if (checarDuplicado) {
-        alert("O item já existe")
+        alert("Item já existe")
     } else {
-        //checar repetidos com arrow function (função anônima)
         listaDeItens.push({
             valor: comprasItem,
             checar: false
@@ -27,45 +22,42 @@ function salvarItem() {
     }
     itensInput.value = ''
 }
-function mostrarItem() { //ForEach manipular obj dentro do array
+function mostrarItem() {
     ulItens.innerHTML = ''
-    ultItensComprados.innerHTML = ''
+    ulItensComprados.innerHTML = ''
     listaDeItens.forEach((elemento, index) => {
         if (elemento.checar) {
-            ultItensComprados.innerHTML +=
-                `<li class="item-compra is-flex is-justify-content-space-between" data-value="${index}">
-            <div>
-                <input type="checkbox" checked class="is-clickable" />
-                <input type="text" class="is-size-5" value="${elemento.valor}"> </input>
-            </div>
-            <div>
-                <i class="fa-solid fa-trash is-clickable deletar"></i>
-            </div>
-        </li>
-            `
+            ulItensComprados.innerHTML += `
+    <li class="item-compra is-flex is-justify-content-space-between" data-value="${index}">
+        <div>
+            <input type="checkbox" checked class="is-clickable" />  
+            <span class="itens-comprados is-size-5">${elemento.valor}</span>
+        </div>
+        <div>
+            <i class="fa-solid fa-trash is-clickable deletar"></i>
+        </div>
+    </li>
+        `
         } else {
             ulItens.innerHTML += `
-        <li class="item-compra is-flex is-justify-content-space-beetween" data-value="${index}">
-            <div>
-                <input type="checkbox" class="is-clickable"/>
-                <input type="text" class="is-size-5" value="${elemento.valor}"></input>
-            </div>
-            <div>
-                <i class="fa-solid fa-trash is-clickable deletar"></i>
-            </div>
-        </li>            
-        `
+    <li class="item-compra is-flex is-justify-content-space-between" data-value="${index}">
+        <div>
+            <input type="checkbox" class="is-clickable" />
+            <input type="text" class="is-size-5" value="${elemento.valor}"></input>
+        </div>
+        <div>
+            <i class="fa-solid fa-trash is-clickable deletar"></i>
+        </div>
+    </li>
+    `
         }
     })
-
-    const inputsCheck = document.querySelectorAll('input[type="checkbox"]');
-    //Seleciona input respectivo
+    const inputsCheck = document.querySelectorAll('input[type="checkbox"]')
     inputsCheck.forEach(i => {
         i.addEventListener('click', (evento) => {
-            const valorDoElemento = (evento.target.parentElement
-                .parentElement.getAttribute('data-value'));
+            valorDoElemento = evento.target.parentElement.parentElement.getAttribute('data-value')
             listaDeItens[valorDoElemento].checar = evento.target.checked
-            mostrarItem();
+            mostrarItem()
         })
     })
 }
